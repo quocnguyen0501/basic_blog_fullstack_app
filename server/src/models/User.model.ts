9dbce02621fa1111ef8afeb652f8c50e1ab52329
quocnguyen.forwork@gmail.com <0909343633Abc>
2022-04-20 08:24:14 +0700

    modify models addding get set and constructor

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -12,28 +12,95 @@ import {
 })
 export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id!: number;
+    private id!: number;
 
     @Column({
         unique: true,
     })
-    email!: string;
+    private email!: string;
 
     @Column()
-    password!: string;
+    private password!: string;
 
     @Column()
-    firstName!: string;
+    private firstName!: string;
 
     @Column()
-    surname!: string;
+    private surname!: string;
 
     @Column()
-    dateOfBirth!: Date;
+    private dateOfBirth!: Date;
 
     @CreateDateColumn()
-    createdAt!: Date;
+    private createdAt!: Date;
 
     @UpdateDateColumn()
-    updatedAt!: Date;
+    private updatedAt!: Date;
+
+    constructor(
+        email: string,
+        password: string,
+        firstName: string,
+        surname: string,
+        dateOfBirth: Date
+    ) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+
+    public getPassword(): string {
+        return this.password;
+    }
+
+    public setPassword(password: string): void {
+        this.password = password;
+    }
+
+    public getFirstName(): string {
+        return this.firstName;
+    }
+
+    public setFirstName(firstName: string): void {
+        this.firstName = firstName;
+    }
+
+    public getSurname(): string {
+        return this.surname;
+    }
+
+    public setSurname(surname: string): void {
+        this.surname = surname;
+    }
+
+    public getDateOfBirth(): Date {
+        return this.dateOfBirth;
+    }
+
+    public setDateOfBirth(dateOfBirth: Date): void {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public getCreatedAt(): Date {
+        return this.createdAt;
+    }
+
+    public getUpdatedAt(): Date {
+        return this.updatedAt;
+    }
 }
