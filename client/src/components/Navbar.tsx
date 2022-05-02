a31f9543dd619273c0a3d2db112820e7e4122f19
quocnguyen0501 <quocnguyen.forwork@gmail.com>
2022-05-02 20:21:13 +0700

    build navbar UI

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -16,10 +16,15 @@ import {
     useColorMode,
     Center,
     Image,
+    Input,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 import logo from "../public/avatar.png";
+import { Formik } from "formik";
+import { ISearchField } from "../types/form/ISearchField";
+import InputSearchField from "./InputSearchField";
 
 const NavLink = ({ children }: { children: ReactNode }) => (
     <Link
@@ -39,6 +44,10 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 export const Navbar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const initialValues: ISearchField = {
+        data: "",
+    };
     return (
         <>
             <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
@@ -47,16 +56,89 @@ export const Navbar = () => {
                     alignItems={"center"}
                     justifyContent={"space-between"}
                 >
-                    <Box>
-                        <Flex>
-                            <Box alignItems={"center"}>
-                                <Image src="https://logos-download.com/wp-content/uploads/2016/06/Reddit_logo_full_1.png" height={"35px"} />
+                    <NextLink href="/">
+                        <button>
+                            <Box w={"180px"}>
+                                <Flex>
+                                    <Box alignItems={"center"}>
+                                        <Image
+                                            src="https://cdn-icons-png.flaticon.com/512/7379/7379126.png"
+                                            height={"30px"}
+                                        />
+                                    </Box>
+                                    <Box
+                                        mx={3}
+                                        fontFamily={"monospace"}
+                                        fontWeight={"semibold"}
+                                        fontSize={"20px"}
+                                    >
+                                        QN-Network
+                                    </Box>
+                                </Flex>
                             </Box>
-                        </Flex>
+                        </button>
+                    </NextLink>
+                    <Box w={"full"} mx={"50px"}>
+                        <Formik
+                            initialValues={initialValues}
+                            onSubmit={(values: ISearchField) =>
+                                console.log(values)
+                            }
+                        >
+                            <InputSearchField
+                                label="Search"
+                                name="Search"
+                                placeholder="Search QN-Network"
+                                type="text"
+                            />
+                        </Formik>
                     </Box>
 
                     <Flex alignItems={"center"}>
                         <Stack direction={"row"} spacing={7}>
+                            <NextLink href="/login">
+                                <Button
+                                    /* flex={1} */
+                                    px={10}
+                                    fontSize={"sm"}
+                                    rounded={"full"}
+                                    bg={"white.400"}
+                                    color={"wblack"}
+                                    border={"solid 0.1px"}
+                                    boxShadow={
+                                        "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                                    }
+                                    _hover={{
+                                        bg: "white.500",
+                                    }}
+                                    _focus={{
+                                        bg: "white.500",
+                                    }}
+                                >
+                                    Log In
+                                </Button>
+                            </NextLink>
+                            <NextLink href="/register">
+                                <Button
+                                    /* flex={1} */
+                                    px={10}
+                                    fontSize={"sm"}
+                                    rounded={"full"}
+                                    bg={"blue.400"}
+                                    color={"white"}
+                                    boxShadow={
+                                        "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                                    }
+                                    _hover={{
+                                        bg: "blue.500",
+                                    }}
+                                    _focus={{
+                                        bg: "blue.500",
+                                    }}
+                                >
+                                    Sign Up
+                                </Button>
+                            </NextLink>
                             <Button onClick={toggleColorMode}>
                                 {colorMode === "light" ? (
                                     <MoonIcon />
