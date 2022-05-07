e0d0d00fe2abd004869932bcb7e2278558a529f5
quocnguyen.forwork@gmail.com <0909343633Abc>
2022-05-07 19:29:08 +0700

    handle change password

--- client/src/pages/change-password.tsx
+++ client/src/pages/change-password.tsx
@@ -1,90 +1,193 @@
+import { CloseIcon, WarningTwoIcon } from "@chakra-ui/icons";
 import {
+    Box,
     Button,
     Flex,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
     Heading,
-    Input,
     Stack,
+    Text,
     useColorModeValue,
 } from "@chakra-ui/react";
-import { Form, Formik, FormikProps } from "formik";
+import { Form, Formik, FormikHelpers, FormikProps } from "formik";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
 import { useState } from "react";
 import { InputPasswordField } from "../components/InputPasswordField";
-import { NewPasswordInput } from "../generated/graphql";
+import {
+    ErrorMutationResponse,
+    NewPasswordInput,
+    useChangePasswordMutation,
+} from "../generated/graphql";
+import { mapFieldErrors } from "../helpers/mapFieldErrors";
 import { validateChangePasswordSchema } from "../validation/ChangePasswordValidationSchema";
+import { useCheckAuth } from "../utils/useCheckAuth";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function ResetPasswordForm(): JSX.Element {
+    const router = useRouter();
+    const { data: dataCheckAuth, loading: loadingCheckAuth } = useCheckAuth();
     const [showPassword, setShowPassword] = useState(false);
+    const [tokenError, setTokenError] = useState("");
     const initialValues: NewPasswordInput = {
         newPassword: "",
     };
     const validationSchema = validateChangePasswordSchema;
+    const [changePassword, _] = useChangePasswordMutation();
 
-    const onChangePasswordSubmit = (values: NewPasswordInput) => {
-        console.log(values);
-        console.log("FINISHHHHHH");
+    const userId = router.query.userId as string;
+    const token = router.query.token as string;
+
+    const onChangePasswordSubmit = async (
+        values: NewPasswordInput,
+        { setErrors }: FormikHelpers<NewPasswordInput>
+    ) => {
+        if (userId && token) {
+            const res = await changePassword({
+                variables: {
+                    userId: userId,
+                    token: token,
+                    newPasswordInput: values,
+                },
+            });
+
+            if (!res.data.changePassword[0].success) {
+                console.log(">>> ERROR", res.data.changePassword[0].code);
+
+                const FIELD_ERROR_NAME_TOKEN = "token";
+
+                const fieldErrors = mapFieldErrors(
+                    (res.data.changePassword[0] as ErrorMutationResponse).errors
+                );
+
+                if (FIELD_ERROR_NAME_TOKEN in fieldErrors) {
+                    setTokenError(fieldErrors.token);
+                }
+                setErrors(fieldErrors);
+            } else {
+                router.push("/login");
+            }
+        }
     };
 
-    return (
-        <>
-            <Flex
-                minH={"100vh"}
-                align={"center"}
-                justify={"center"}
-                bg={useColorModeValue("gray.50", "gray.800")}
-            >
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={onChangePasswordSubmit}
-                >
-                    {({
-                        isSubmitting,
-                    }: FormikProps<NewPasswordInput>) => (
-                        <Form>
-                            <Stack
-                                spacing={4}
-                                w={"full"}
-                                maxW={"md"}
-                                bg={useColorModeValue("white", "gray.700")}
-                                rounded={"xl"}
-                                boxShadow={"lg"}
-                                p={6}
-                                my={12}
+    if (
+        loadingCheckAuth ||
+        (!loadingCheckAuth && dataCheckAuth?.loginProfile)
+    ) {
+        return <LoadingSpinner />;
+    } else if (!userId || !token) {
+        router.replace("/");
+    } else {
+        return (
+            <>
+                {tokenError ? (
+                    <Box textAlign="center" py={10} px={6}>
+                        <Box display="inline-block">
+                            <Flex
+                                flexDirection="column"
+                                justifyContent="center"
+                                alignItems="center"
+                                bg={"red.500"}
+                                rounded={"50px"}
+                                w={"55px"}
+                                h={"55px"}
+                                textAlign="center"
                             >
-                                <Heading
-                                    lineHeight={1.1}
-                                    fontSize={{ base: "2xl", md: "3xl" }}
-                                >
-                                    Enter new password
-                                </Heading>
-                                <InputPasswordField
-                                    name="newPassword"
-                                    placeholder="Enter new password"
-                                    label="Password"
-                                    showPassword={showPassword}
-                                    setShowPassword={setShowPassword}
-                                />
-                                <Stack spacing={6}>
-                                    <Button
-                                        type="submit"
-                                        bg={"blue.400"}
-                                        color={"white"}
-                                        _hover={{
-                                            bg: "blue.500",
-                                        }}
-                                        isLoading={isSubmitting}
+                                <CloseIcon boxSize={"20px"} color={"white"} />
+                            </Flex>
+                        </Box>
+                        <Heading as="h2" size="xl" mt={6} mb={2}>
+                            TOKEN ERROR
+                        </Heading>
+                        <Text color={"gray.500"}>{tokenError}</Text>
+                        <NextLink href={"/forgot-password"}>
+                            <Button
+                                /* flex={1} */
+                                mt={5}
+                                px={8}
+                                py={2}
+                                fontSize={"sm"}
+                                rounded={"full"}
+                                bg={"red.400"}
+                                color={"white"}
+                                boxShadow={
+                                    "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                                }
+                                _hover={{
+                                    bg: "red.500",
+                                }}
+                                _focus={{
+                                    bg: "red.500",
+                                }}
+                            >
+                                Go back to Forgot Password
+                            </Button>
+                        </NextLink>
+                    </Box>
+                ) : (
+                    <Flex
+                        minH={"100vh"}
+                        align={"center"}
+                        justify={"center"}
+                        bg={useColorModeValue("gray.50", "gray.800")}
+                    >
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={onChangePasswordSubmit}
+                        >
+                            {({
+                                isSubmitting,
+                            }: FormikProps<NewPasswordInput>) => (
+                                <Form>
+                                    <Stack
+                                        spacing={4}
+                                        w={"full"}
+                                        maxW={"md"}
+                                        bg={useColorModeValue(
+                                            "white",
+                                            "gray.700"
+                                        )}
+                                        rounded={"xl"}
+                                        boxShadow={"lg"}
+                                        p={6}
+                                        my={12}
                                     >
-                                        Submit
-                                    </Button>
-                                </Stack>
-                            </Stack>
-                        </Form>
-                    )}
-                </Formik>
-            </Flex>
-        </>
-    );
+                                        <Heading
+                                            lineHeight={1.1}
+                                            fontSize={{
+                                                base: "2xl",
+                                                md: "3xl",
+                                            }}
+                                        >
+                                            Enter new password
+                                        </Heading>
+                                        <InputPasswordField
+                                            name="newPassword"
+                                            placeholder="Enter new password"
+                                            label="Password"
+                                            showPassword={showPassword}
+                                            setShowPassword={setShowPassword}
+                                        />
+                                        <Stack spacing={6}>
+                                            <Button
+                                                type="submit"
+                                                bg={"blue.400"}
+                                                color={"white"}
+                                                _hover={{
+                                                    bg: "blue.500",
+                                                }}
+                                                isLoading={isSubmitting}
+                                            >
+                                                Submit
+                                            </Button>
+                                        </Stack>
+                                    </Stack>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Flex>
+                )}
+            </>
+        );
+    }
 }
--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -10,19 +10,23 @@ import {
     useColorModeValue,
     FormErrorMessage,
     Box,
+    Spinner,
 } from "@chakra-ui/react";
 import { Form, Formik, FormikProps } from "formik";
 import NextLink from "next/link";
 import React from "react";
 import InputTextField from "../components/InputTextField";
+import LoadingSpinner from "../components/LoadingSpinner";
 import {
     ForgotPasswordInput,
     useForgotPasswordMutation,
 } from "../generated/graphql";
+import { useCheckAuth } from "../utils/useCheckAuth";
 import { validateForgotPasswordSchema } from "../validation/ForgotPasswordValidationSchema";
 
 const ForgotPassword = () => {
     const [forgotPassword, { loading, data }] = useForgotPasswordMutation();
+    const { data: dataCheckAuth, loading: loadingCheckAuth } = useCheckAuth();
 
     const initialValues: ForgotPasswordInput = {
         email: "",
@@ -32,129 +36,146 @@ const ForgotPassword = () => {
 
     const onForgotPasswordSubmit = async (values: ForgotPasswordInput) => {
         console.log(">>> EMAIL: ", values);
-        
+
         await forgotPassword({
             variables: {
                 forgotPasswordInput: values,
             },
         });
     };
-    return (
-        <>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={onForgotPasswordSubmit}
-            >
-                {({
-                    values,
-                    touched,
-                    errors,
-                    handleChange,
-                    isSubmitting,
-                }: FormikProps<ForgotPasswordInput>) =>
-                    !loading && data ? (
-                        <Box textAlign="center" py={10} px={6}>
-                            <CheckCircleIcon
-                                boxSize={"50px"}
-                                color={"green.500"}
-                            />
-                            <Heading as="h2" size="xl" mt={6} mb={2}>
-                                Verify your email address
-                            </Heading>
-                            <Text color={"gray.500"} mb={"13px"}>
-                                Thanks for using my service. Please confirm your
-                                email address to change password!
-                            </Text>
-                            <NextLink href="https://mail.google.com">
-                                <Button
-                                    /* flex={1} */
-                                    px={10}
-                                    fontSize={"sm"}
-                                    rounded={"full"}
-                                    bg={"blue.400"}
-                                    color={"white"}
-                                    boxShadow={
-                                        "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-                                    }
-                                    _hover={{
-                                        bg: "blue.500",
-                                    }}
-                                    _focus={{
-                                        bg: "blue.500",
-                                    }}
-                                >
-                                    Verify Email
-                                </Button>
-                            </NextLink>
-                        </Box>
-                    ) : (
-                        <Form>
-                            <Flex
-                                minH={"100vh"}
-                                align={"center"}
-                                justify={"center"}
-                                bg={useColorModeValue("gray.50", "gray.800")}
-                            >
-                                <Stack
-                                    spacing={4}
-                                    w={"full"}
-                                    maxW={"md"}
-                                    bg={useColorModeValue("white", "gray.700")}
-                                    rounded={"xl"}
-                                    boxShadow={"lg"}
-                                    p={6}
-                                    my={12}
-                                >
-                                    <Heading
-                                        lineHeight={1.1}
-                                        fontSize={{ base: "2xl", md: "3xl" }}
+
+    if (
+        loadingCheckAuth ||
+        (!loadingCheckAuth && dataCheckAuth?.loginProfile)
+    ) {
+        return <LoadingSpinner />;
+    } else {
+        return (
+            <>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onForgotPasswordSubmit}
+                >
+                    {({
+                        values,
+                        touched,
+                        errors,
+                        handleChange,
+                        isSubmitting,
+                    }: FormikProps<ForgotPasswordInput>) =>
+                        !loading && data ? (
+                            <Box textAlign="center" py={10} px={6}>
+                                <CheckCircleIcon
+                                    boxSize={"50px"}
+                                    color={"green.500"}
+                                />
+                                <Heading as="h2" size="xl" mt={6} mb={2}>
+                                    Find Your Account
+                                </Heading>
+                                <Text color={"gray.500"} mb={"13px"}>
+                                    Please enter your email address to search
+                                    for your account.
+                                </Text>
+                                <NextLink href="https://mail.google.com">
+                                    <Button
+                                        /* flex={1} */
+                                        px={10}
+                                        fontSize={"sm"}
+                                        rounded={"full"}
+                                        bg={"blue.400"}
+                                        color={"white"}
+                                        boxShadow={
+                                            "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                                        }
+                                        _hover={{
+                                            bg: "blue.500",
+                                        }}
+                                        _focus={{
+                                            bg: "blue.500",
+                                        }}
                                     >
-                                        Forgot your password?
-                                    </Heading>
-                                    <Text
-                                        fontSize={{ base: "sm", sm: "md" }}
-                                        color={useColorModeValue(
-                                            "gray.800",
-                                            "gray.400"
+                                        Verify Email
+                                    </Button>
+                                </NextLink>
+                            </Box>
+                        ) : (
+                            <Form>
+                                <Flex
+                                    minH={"100vh"}
+                                    align={"center"}
+                                    justify={"center"}
+                                    bg={useColorModeValue(
+                                        "gray.50",
+                                        "gray.800"
+                                    )}
+                                >
+                                    <Stack
+                                        spacing={4}
+                                        w={"full"}
+                                        maxW={"md"}
+                                        bg={useColorModeValue(
+                                            "white",
+                                            "gray.700"
                                         )}
+                                        rounded={"xl"}
+                                        boxShadow={"lg"}
+                                        p={6}
+                                        my={12}
                                     >
-                                        You&apos;ll get an email with a reset
-                                        link
-                                    </Text>
-                                    <InputTextField
-                                        name="email"
-                                        placeholder="your-email@example.com"
-                                        label="Email address"
-                                        type="text"
-                                    />
-                                    {errors.email && touched.email ? (
-                                        <FormErrorMessage>
-                                            {errors.email}
-                                        </FormErrorMessage>
-                                    ) : null}
-                                    <Stack spacing={6}>
-                                        <Button
-                                            type="submit"
-                                            size="lg"
-                                            bg={"blue.400"}
-                                            color={"white"}
-                                            _hover={{
-                                                bg: "blue.500",
+                                        <Heading
+                                            lineHeight={1.1}
+                                            fontSize={{
+                                                base: "2xl",
+                                                md: "3xl",
                                             }}
-                                            isLoading={isSubmitting}
                                         >
-                                            Request Reset
-                                        </Button>
+                                            Forgot your password?
+                                        </Heading>
+                                        <Text
+                                            fontSize={{ base: "sm", sm: "md" }}
+                                            color={useColorModeValue(
+                                                "gray.800",
+                                                "gray.400"
+                                            )}
+                                        >
+                                            You&apos;ll get an email with a
+                                            reset link
+                                        </Text>
+                                        <InputTextField
+                                            name="email"
+                                            placeholder="your-email@example.com"
+                                            label="Email address"
+                                            type="text"
+                                        />
+                                        {errors.email && touched.email ? (
+                                            <FormErrorMessage>
+                                                {errors.email}
+                                            </FormErrorMessage>
+                                        ) : null}
+                                        <Stack spacing={6}>
+                                            <Button
+                                                type="submit"
+                                                size="lg"
+                                                bg={"blue.400"}
+                                                color={"white"}
+                                                _hover={{
+                                                    bg: "blue.500",
+                                                }}
+                                                isLoading={isSubmitting}
+                                            >
+                                                Request Reset
+                                            </Button>
+                                        </Stack>
                                     </Stack>
-                                </Stack>
-                            </Flex>
-                        </Form>
-                    )
-                }
-            </Formik>
-        </>
-    );
+                                </Flex>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </>
+        );
+    }
 };
 
 export default ForgotPassword;
