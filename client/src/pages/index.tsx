27bf95243f4016aeaf4fd967679ef8bf8b240b00
quocnguyen0501 <quocnguyen.forwork@gmail.com>
2022-05-10 10:46:07 +0700

    add new field for usePostQuery(fetchMore, networkStatus) and set up for load more post click

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
     Stack,
     Avatar,
     useColorModeValue,
+    Button,
 } from "@chakra-ui/react";
 import LoadingSpinner from "../components/LoadingSpinner";
 import Navbar from "../components/Navbar";
@@ -14,20 +15,39 @@ import { Post, PostsDocument, usePostsQuery } from "../generated/graphql";
 import { addApolloState, initializeApollo } from "../lib/apolloClient";
 import SinglePost from "../components/SinglePost";
 import { FC } from "react";
+import { NetworkStatus } from "@apollo/client";
 
 const LIMIT = 5;
 
 const Index: FC = () => {
-    const { data, loading } = usePostsQuery({
+    /**
+     * Call in Cache Apollo not send request to server
+     */
+    const { data, loading, error, fetchMore, networkStatus } = usePostsQuery({
         variables: {
             limit: LIMIT,
         },
+        /**
+         * Component co render boi usePostsQuery hook, se rerender khi networkStatus thay doi,
+         * => fetchMore: function recall query for get more data
+         */
+        notifyOnNetworkStatusChange: true,
     });
+
+    const isLoadingMorePost = networkStatus === NetworkStatus.fetchMore;
+
+    const loadMorePost = () =>
+        fetchMore({
+            variables: {
+                limit: LIMIT,
+                timeCompareCreatedAt: data?.posts?.timeCompareCreatedAt,
+            },
+        });
 
     return (
         <>
             <Navbar />
-            {loading ? (
+            {loading && !isLoadingMorePost ? (
                 <LoadingSpinner />
             ) : (
                 <>
@@ -36,6 +56,29 @@ const Index: FC = () => {
                             {data?.posts.paginatedPosts.map((post: Post) => (
                                 <SinglePost key={post.id} post={post} />
                             ))}
+                            <Center>
+                                {data.posts.hasMore && (
+                                    <Button
+                                        fontSize={"sm"}
+                                        rounded={"full"}
+                                        bg={"blue.400"}
+                                        color={"white"}
+                                        boxShadow={
+                                            "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                                        }
+                                        _hover={{
+                                            bg: "blue.500",
+                                        }}
+                                        _focus={{
+                                            bg: "blue.500",
+                                        }}
+                                        isLoading={isLoadingMorePost}
+                                        onClick={loadMorePost}
+                                    >
+                                        {isLoadingMorePost ? 'Loading' : 'Show more'}
+                                    </Button>
+                                )}
+                            </Center>
                         </Box>
                     </Center>
                 </>
